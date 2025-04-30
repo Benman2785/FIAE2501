27c6d30e881a8f3ef9ae1e88170ebd328db78104
Benman2785 <benman2785@hotmail.de>
2025-04-30 13:18:30 +0200

    Add files via upload

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,16 +1,40 @@
+
 public class FizzBuzz {
     public static void main(String[] args) {
 
-        for (int i = 1; i <=30; i++) {
-            if(i % 3 == 0 && i % 5 == 0){
-                System.out.println("FizzBuzz");
-            } else if(i % 3 == 0){
-                System.out.println("Fizz");
-            } else if(i % 5 == 0){
-                System.out.println("Buzz");
-            }else{
-                System.out.println(i);
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+        System.out.println("-=[ Knaller FizzBuzz Line ]=-");
+
+        // ArrayList<Integer> zahlen = new ArrayList<>();
+        // for (int i = 1; i < 31; i++) {
+        //     zahlen.add(i);
+        // }
+
+        // for (int i= 0; i < 30; i++){
+        //     if (zahlen.get(i) % 5 == 0 && zahlen.get(i) % 3 == 0) {
+        //         System.out.print("FizzBuzz ");
+        //     } else if (zahlen.get(i) % 3 == 0) {
+        //         System.out.print("Fizz ");
+        //     } else if (zahlen.get(i) % 5 == 0) {
+        //         System.out.print("Buzz ");
+        //     } else {
+        //         System.err.print(zahlen.get(i) + " ");
+        //     }
+        // }
+
+        for (int i= 1; i < 31; i++){
+            if (i % 5 == 0 && i % 3 == 0) {
+                System.out.print("FizzBuzz ");
+            } else if (i % 3 == 0) {
+                System.out.print("Fizz ");
+            } else if (i % 5 == 0) {
+                System.out.print("Buzz ");
+            } else {
+                System.err.print(i + " ");
             }
         }
+        System.out.println(" ");
+        System.out.println("-=[ Knaller Ändä ]=-");
     }
 }
--- JedeZweiteZahl.java
+++ JedeZweiteZahl.java
@@ -1,8 +1,7 @@
 public class JedeZweiteZahl {
     public static void main(String[] args) {
-        
-        for(int i = 100; i >= 10; i = i - 2){
-            System.out.println(i);
+        for (int i = 10; i <= 100; i = i + 2) {
+            System.out.print(i + " ");
         }
     }
 }
