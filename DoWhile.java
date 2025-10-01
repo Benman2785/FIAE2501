91d6b4a780b4288277768beb843118497a1db32a
Benman2785 <benman2785@hotmail.de>
2025-10-01 08:53:21 +0200

    ???
    ???

--- DoWhile.java
+++ DoWhile.java
@@ -1,19 +1,27 @@
-import java.util.Scanner;
-
-public class DoWhile {
+import java.util.*;
+public class Dowhile {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int eingabe = 0;
-        
-        do {
-            System.out.println("Aufstehen:      Push (1)");
-            System.out.println("Kaffee trinken: Push (2)");
-            System.out.println("Schlafen        Push (3)");
-            System.out.println("Ende            Push (4)");
 
-            System.out.print("Ihre Eingabe bitte ");
-            eingabe = sc.nextInt();  
-        } 
-        while (eingabe > 0 && eingabe < 4);
+        do {
+        System.out.println("1 - Aufstehen");
+        System.out.println("2 - Kaffee trinken");
+        System.out.println("3 - Schlafen");
+        System.out.println("4 - Ende");
+
+        System.out.print("Ihre Eingabe: ");
+        eingabe = sc.nextInt();
+        if (eingabe == 1) {
+            System.out.println("KIKERIKI!!!");
+            // break;
+        } else if (eingabe == 2) {
+            System.out.println("ECHT JETZT! SO RICHTIG HEISSER SCHWARZER KAFFEE JUNGE!");
+            // break;
+        } else if (eingabe == 3) {
+            System.out.println("Schaefchen zaehlen! JETZT!");
+            // break;
+        }
+        } while (eingabe > 0 && eingabe < 4);
     }
 }
--- Zufallszahlen.java
+++ Zufallszahlen.java
@@ -1,10 +1,15 @@
 import java.util.Random;
-public class Zufallszahlen {
+
+public class ZufallsZahlen {
     public static void main(String[] args) {
+
         Random random = new Random();
-        int a = random.nextInt(100);
-        int b = random.nextInt(100) + 1;
-        int c = random.nextInt(-10,10);
+
+        int a = random.nextInt(100); // Zufallszahl von 0-99
+        int aa = random.nextInt(101); // Zufallszahl von 0-100
+        int b = random.nextInt(-1,101); // Zufallszahl von -1 bis 100
+        int c = random.nextInt(-10,10); // Zufallszahl von -10 bis 9
+
         System.out.println(a);
         System.out.println(b);
         System.out.println(c);
