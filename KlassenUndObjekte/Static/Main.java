dbc1f0f596b6dff0a6ee6928edc27752c3a5c1a5
Benman2785 <benman2785@hotmail.de>
2025-10-10 15:01:42 +0200

    Delete KlassenUndObjekte directory

--- KlassenUndObjekte/Static/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Konfiguration.setStandardSprache("en");
-        System.out.println(Konfiguration.getStandardSprache());
-    }    
-}
